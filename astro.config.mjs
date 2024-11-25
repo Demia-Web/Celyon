4794ccecf14724c685639d72b0f489950ba91e39
Niko71 <nicolobassi58@gmail.com>
2024-11-25 15:29:59 +0100

    0.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import lenis from "astro-lenis";
-
 import astroI18next from "astro-i18next";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), astroI18next()]
