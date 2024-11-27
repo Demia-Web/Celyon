4a1d0e4e32ed0544f7910ef3d171a8f573aa68e6
Niko71 <nicolobassi58@gmail.com>
2024-11-27 12:31:26 +0100

    0.6.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
 import astroI18next from "astro-i18next";
+
+import node from "@astrojs/node";
+
+// import transitions from "astro-transitions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next()]
+  integrations: [tailwind(), astroI18next()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
