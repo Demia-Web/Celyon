bbc0399a61e3deef431da066d2ec7b0d5c8062ba
Niko <nicolobassi58@gmail.com>
2024-12-18 12:34:28 +0100

    0.6.9 | Netfly integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import astroI18next from "astro-i18next";
 
 import node from "@astrojs/node";
 
+import netlify from "@astrojs/netlify";
+
 // import transitions from "astro-transitions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
