7243370eda3dd3c82f412a09e475354f8f08b27c
Niko <nicolobassi58@gmail.com>
2024-12-18 16:23:21 +0100

    0.7.5

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 
+// Importa i file di traduzione per IT e EN
+import it from "./public/locales/it/translation.json" assert { type: "json" };
+import en from "./public/locales/en/translation.json" assert { type: "json" };
+
 export default {
   defaultLocale: "it",
   locales: ["it", "en"],
-  returnObjects: true
+  returnObjects: true,
+  i18nextServer: {
+    resources: {
+      it: {
+        translation: {
+          ...it
+        }
+      },
+      en: {
+        translation: {
+          ...en
+        }
+      }
+    }
+  }
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,8 @@ import astroI18next from "astro-i18next";
 
 import netlify from "@astrojs/netlify";
 
-// import node from "@astrojs/node";
-
-// import transitions from "astro-transitions";
-
-// import it from "./public/locales/it/translation.json" assert { type: "json" };
-// import en from "./public/locales/en/translation.json" assert { type: "json" };
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    astroI18next({
-      defaultLocale: "it",
-      locales: ["it", "en"],
-      returnObjects: true
-    })
-  ],
+  integrations: [tailwind(), astroI18next()],
   adapter: netlify(),
   output: "server"
 });
